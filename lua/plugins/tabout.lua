f7b908d0bb22fabf0a6633e1f8eb913232074441
robbow <rob.bowland@gmail.com>
2025-05-24 22:49:41 +0100

    Remove unused dependency

--- lua/plugins/tabout.lua
+++ lua/plugins/tabout.lua
@@ -26,18 +26,10 @@ return {
     end,
     dependencies = { -- These are optional
       "nvim-treesitter/nvim-treesitter",
-      "L3MON4D3/LuaSnip",
       "hrsh7th/nvim-cmp",
     },
     opt = true, -- Set this to true if the plugin is optional
     event = "InsertCharPre", -- Set the event to 'InsertCharPre' for better compatibility
     priority = 1000,
-  },
-  {
-    "L3MON4D3/LuaSnip",
-    keys = function()
-      -- Disable default tab keybinding in LuaSnip
-      return {}
-    end,
   },
 }
