41578432744ed9fb9f1ec2d86c35abccb678790f
robbow <rob.bowland@gmail.com>
2025-05-28 22:43:45 +0100

    Add some custom keybindings
    Some of these are common rebindings, others are to alleviate some personal frustrations.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode" })
+
+vim.keymap.set("n", "H", "^", { desc = "Move to beginning of line" })
+vim.keymap.set("n", "L", "$", { desc = "Move to end of line" })
+
+vim.keymap.set({ "n", "v" }, "x", '"_x', { desc = "Delete character without yanking" })
+vim.keymap.set({ "n", "v" }, "d", '"_d', { desc = "Delete without yanking" })
+vim.keymap.set({ "n", "v" }, "c", '"_c', { desc = "Change without yanking" })
+
+-- Cmd + S : I constantly hit this from muscle memory, so why not?
+vim.keymap.set({ "n", "i", "v" }, "<D-s>", "<Cmd>w<CR>", { desc = "Save file" })
