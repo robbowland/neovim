b8d8a485d7667d584b37a40fd0af00f080b2d210
Rob Bowland <me@robbow.land>
2025-09-11 10:16:52 +0100

    Pin `neotest` to working commit
    Later commits are not working with `neotest-jest`, reporting 'tests not found' when attempting to run.

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,5 +1,8 @@
 return {
   "nvim-neotest/neotest",
+  -- Workaround for 'tests not found' with neotest-jest due to broken neotest
+  -- Issue: https://github.com/nvim-neotest/neotest/issues/531
+  commit = "52fca6717ef972113ddd6ca223e30ad0abb2800c",
   opts = { adapters = { "neotest-jest" } },
   dependencies = { "haydenmeade/neotest-jest" },
 }
