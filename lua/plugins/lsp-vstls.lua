de43e111aad122538645b1002aee208dfdab7c6a
robbowland <me@robbow.land>
2025-11-01 11:54:14 +0000

    Attempt to improve `vtls` stability
    It keeps crashing in large typescript projects.
    Let's see if this improves things.

--- lua/plugins/lsp-vstls.lua
+++ lua/plugins/lsp-vstls.lua
@@ -3,8 +3,18 @@ return {
   opts = {
     servers = {
       vtsls = {
+        cmd_env = {
+          -- Allow the language server to allocate more memory in large workspaces
+          NODE_OPTIONS = "--max-old-space-size=6144",
+        },
         settings = {
           typescript = {
+            tsserver = {
+              -- Disable the extra syntax server so we only spawn one tsserver process
+              useSeparateSyntaxServer = false,
+              -- Raise the memory ceiling to reduce out-of-memory crashes
+              maxTsServerMemory = 6144,
+            },
             inlayHints = {
               enumMemberValues = { enabled = true },
               functionLikeReturnTypes = { enabled = true },
@@ -12,6 +22,12 @@ return {
               parameterTypes = { enabled = true },
               propertyDeclarationTypes = { enabled = true },
               variableTypes = { enabled = true },
+            },
+          },
+          javascript = {
+            tsserver = {
+              useSeparateSyntaxServer = false,
+              maxTsServerMemory = 6144,
             },
           },
         },
