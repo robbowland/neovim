7e42149149c8125a131aef4e0ac04c614724c440
robbowland <me@robbow.land>
2025-11-11 16:13:17 +0000

    Use custom `gitui` fork

--- lua/plugins/gitui-keymaps.lua
+++ lua/plugins/gitui-keymaps.lua
@@ -1,18 +1,27 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    optional = true,
-    keys = function(_, keys)
-      for _, key in ipairs(keys) do
-        if key[1] == "<leader>gg" then
-          key[1] = "<leader>gc"
-          key.desc = "Git Commit (Root Dir)"
-        elseif key[1] == "<leader>gG" then
-          key[1] = "<leader>gC"
-          key.desc = "Git Commit (cwd)"
-        end
-      end
-      return keys
-    end,
-  },
+  "mason-org/mason.nvim",
+  opts = { ensure_installed = {} },
+
+  init = function()
+    local GITUI = "/opt/homebrew/bin/gitui"
+    if vim.fn.executable(GITUI) ~= 1 then
+      GITUI = "gitui" -- fallback
+    end
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "LazyVimKeymaps",
+      once = true,
+      callback = function()
+        -- <leader>gc = Git Commit (Root Dir)
+        vim.keymap.set("n", "<leader>gc", function()
+          Snacks.terminal({ GITUI }, { cwd = LazyVim.root.get() })
+        end, { desc = "Git Commit (Root Dir)" })
+
+        -- <leader>gC = Git Commit (cwd)
+        vim.keymap.set("n", "<leader>gC", function()
+          Snacks.terminal({ GITUI })
+        end, { desc = "Git Commit (cwd)" })
+      end,
+    })
+  end,
 }
